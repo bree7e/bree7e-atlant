7a5e132bcd6226f2b48426519b21a5fbcd3f66d6
Alexandr Vetrov <vetrov@icc.ru>
2018-04-29 09:34:23 +0800

    lazy loading

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HomePage from '@/components/HomePage'
-import FractionsPage from '@/components/FractionsPage'
-import CommentsPage from '@/components/CommentsPage'
+// import HomePage from '@/components/HomePage'
+// import FractionsPage from '@/components/FractionsPage'
+// import CommentsPage from '@/components/CommentsPage'
+const HomePage = () => import('@/components/HomePage')
+const FractionsPage = () => import('@/components/FractionsPage')
+const CommentsPage = () => import('@/components/CommentsPage')
 
 Vue.use(Router)
 
