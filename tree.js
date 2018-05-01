fd1d48d4cbca644481cddd81ed09e03e11f74403
Alexandr Vetrov <vetrov@icc.ru>
2018-05-01 23:30:27 +0800

    Дерево собирается правильно

--- tree.js
+++ tree.js
@@ -1,117 +1,46 @@
-// https://code.tutsplus.com/articles/data-structures-with-javascript-tree--cms-23393
-// http://www.internet-technologies.ru/articles/strukturirovanie-dannyh-s-pomoschyu-javascript-derevo.html
-
-function Tree (node) {
-  this._root = node
+class Node {
+  constructor (data) {
+    this.data = data
+    this.left = null
+    this.right = null
+  }
 }
 
-function Node (data) {
-  this.data = data
-  this.parent = null
-  this.left = null
-  this.right = null
-}
+class PartTree {
+  constructor (part) {
+    const newNode = new Node(part.fraction)
+    this.root = newNode
+    this.lastOperator = null
+    this.lastOperand = newNode
+  }
 
-Tree.prototype.add = function(data, toNode) {
-  const newNode = new Node(data)
-  if (['+', '-', '*', '/'].indexOf(data) > -1) {
-    newNode.left = toNode
-    if (toNode.parent === null) {
-      this._root = newNode
-    } else {
-      toNode.parent.right = newNode
-      newNode.parent = toNode.parent
+  addPart (part) {
+    const newOperator = new Node(part.sign)
+    const newOperand = new Node(part.fraction)
+    newOperator.right = newOperand
+
+    switch (part.sign) {
+      case '+':
+      case '-':
+        newOperator.left = this.root
+        this.root = newOperator
+        break
+      case '*':
+      case '/':
+        newOperator.left = this.lastOperand
+        if (this.lastOperator) this.lastOperator.right = newOperator
+        break
     }
-    toNode.parent = newNode
-  } else {
-    newNode.parent = toNode
-    toNode.right = newNode
+
+    this.lastOperator = newOperator
+    this.lastOperand = newOperand
   }
-  return newNode    
-};
-  
-/*
-  2+3*4
-
-  +
-  ├── 2
-  └── *
-      ├── 3
-      └── 4
-*/
-const twoR = new Node(2)
-let exp = new Tree(twoR)
-const plusR = exp.add('+', twoR)
-const threeR = exp.add(3, plusR)
-const multiR = exp.add('*', threeR)
-const fourR = exp.add(4, multiR)
-
-console.log(exp);
-  
-let toNode = null
-
-const two = new Node(2)
-toNode = two
-let expr = new Tree(two); // {_root: Node {data: 2, parent: null, left: null, right: null }}
-
-const plusNode = new Node('+') // операция ставится рутом последней левой ноды
-plusNode.left = toNode
-if (toNode.parent === null) {
-    expr._root = plusNode
-} else {
-    toNode.parent.right = plusNode
-    plusNode.parent = toNode.parent
 }
-toNode.parent = plusNode
-toNode = plusNode
+ 
+let exp = new PartTree({sign: '', fraction: '2'})
+exp.addPart({sign: '+', fraction: '3'})
+exp.addPart({sign: '+', fraction: '4'})
+exp.addPart({sign: '*', fraction: '5'})
+exp.addPart({sign: '-', fraction: '7'})
 
-const three = new Node(3)
-three.parent = toNode
-toNode.right = three
-toNode = three
-
-const multiNode = new Node('*') 
-multiNode.left = toNode
-if (toNode.parent === null) {
-    expr._root = multiNode
-} else {
-    toNode.parent.right = multiNode
-    multiNode.parent = toNode.parent
-}
-toNode.parent = multiNode
-toNode = multiNode
-
-const four = new Node(4)
-four.parent = toNode
-toNode.right = four
-toNode = four
-
-function isEquivalent(a, b) {
-  // Create arrays of property names
-  var aProps = Object.getOwnPropertyNames(a);
-  var bProps = Object.getOwnPropertyNames(b);
-
-  // If number of properties is different,
-  // objects are not equivalent
-  if (aProps.length != bProps.length) {
-      return false;
-  }
-
-  for (var i = 0; i < aProps.length; i++) {
-      var propName = aProps[i];
-
-      // If values of same property are not equal,
-      // objects are not equivalent
-      if (a[propName] !== b[propName]) {
-          return false;
-      }
-  }
-
-  // If we made it this far, objects
-  // are considered equivalent
-  return true;
-}
-
-console.log(expr)
-
-console.log(isEquivalent(exp,expr));
+console.log(exp)
