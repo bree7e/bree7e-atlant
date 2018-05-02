1cb39401702f6e096daa275c1276924216838993
Alexandr Vetrov <vetrov@icc.ru>
2018-05-02 10:50:13 +0800

    Дроби без * и /

--- tree.js
+++ tree.js
@@ -51,32 +51,68 @@ class PartTree {
     if (node.left) {
       left = this.calculate(node.left)
     } else {
-      return Number(node.data)
+      return node.data
     }
-    switch (node.data) {
-      case '+':
-        return left + this.calculate(node.right)
-      case '-':
-        return left - this.calculate(node.right)
-      case '*':
-        return left * this.calculate(node.right)
-      case '/':
-        return left / this.calculate(node.right)
-    }    
+    const right = node.right.data
+    return this.calculateFractions(left, right, node.data)
+    // switch (node.data) {
+    //   case '+':
+    //     return left + this.calculate(node.right)
+    //   case '-':
+    //     return left - this.calculate(node.right)
+    //   case '*':
+    //     return left * this.calculate(node.right)
+    //   case '/':
+    //     return left / this.calculate(node.right)
+    // }    
   }
+
+  calculateFractions(a, b, operator) {
+    let numerator = 0
+    let denominator = 0
+    switch (operator) {
+      case '+':
+        numerator = (a.numerator * b.denominator) + (b.numerator * a.denominator)
+        denominator = a.denominator * b.denominator
+        break
+      case '-':
+        numerator = (a.numerator * b.denominator) - (b.numerator * a.denominator)
+        denominator = a.denominator * b.denominator
+        break
+      case '*':
+        numerator = a.numerator * b.numerator
+        denominator = a.denominator * b.denominator
+        break
+      case '/':
+        numerator = a.numerator * b.denominator
+        denominator = a.denominator * b.numerator
+        break
+    }
+    return {
+      numerator: numerator,
+      denominator: denominator
+    }
+  }  
 
   result () {
     return this.calculate(this.root)
   }
 }
  
-let exp = new PartTree({sign: '', fraction: '2'})
-exp.addPart({sign: '+', fraction: '3'})
-exp.addPart({sign: '+', fraction: '4'})
-exp.addPart({sign: '*', fraction: '5'})
-exp.addPart({sign: '-', fraction: '7'})
-// let exp = new PartTree({sign: '', fraction: '2'})
-// exp.addPart({sign: '+', fraction: '2'})
-// exp.addPart({sign: '*', fraction: '2'})
+let exp = new PartTree({sign: '', fraction: {  numerator: 3,  denominator: 7 }})
+exp.addPart({sign: '-', fraction:  {  numerator: 2,  denominator: 5 }})
+exp.addPart({sign: '+', fraction:  {  numerator: 1,  denominator: 35 }})
+// exp.addPart({sign: '*', fraction:  {  numerator: 1,  denominator: 2 }})
+// exp.addPart({sign: '+', fraction:  {  numerator: 1,  denominator: 2 }})
+// exp.addPart({sign: '+', fraction:  {  numerator: 1,  denominator: 2 }})
+
+// let exp = new PartTree({sign: '', fraction: '5'})
+// exp.addPart({sign: '/', fraction: '7'})
+// exp.addPart({sign: '*', fraction: '7'})
+// exp.addPart({sign: '+', fraction: '4'})
+// exp.addPart({sign: '+', fraction: '8'})
+// exp.addPart({sign: '*', fraction: '4'})
+// exp.addPart({sign: '/', fraction: '9'})
+// exp.addPart({sign: '-', fraction: '30'})
 
 console.log(exp.result())
